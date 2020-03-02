cc367db0941ac9d9c74fc99ca563f551fee7e344
Jukka Juslin <jukka.juslin@haaga-helia.fi>
2020-03-02 13:51:35 +0200

    Update GPRS_SMSread.ino

--- examples/GPRS_SMSread/GPRS_SMSread.ino
+++ examples/GPRS_SMSread/GPRS_SMSread.ino
@@ -1,19 +1,3 @@
-/*
-    GPRS SMS Read
-
-    This sketch is used to test seeeduino GPRS_Shield's reading SMS
-    function.To make it work, you should insert SIM card
-    to Seeeduino GPRS Shield,enjoy it!
-
-    There are two methods to read SMS:
-    1. GPRS_LoopHandle.ino  -> in order to recieve "+CMTI: "SM""
-      may be you need to send this command to your shield: "AT+CNMI=2,2,0,0,0"
-    2. GPRS_SMSread.ino -> you have to check if there are any
-      UNREAD sms, and you don't need to check serial data continuosly
-
-    create on 2015/05/14, version: 1.0
-    by op2op2op2(op2op2op2@hotmail.com)
-*/
 
 #include <GPRS_Shield_Arduino.h>
 #include <SoftwareSerial.h>
@@ -22,6 +6,8 @@
 #define PIN_TX    7
 #define PIN_RX    8
 #define BAUDRATE  9600
+#define PHONE_NUMBER "0405209879"
+#define MESSAGE  "starting automatic water system"
 
 #define MESSAGE_LENGTH 160
 char message[MESSAGE_LENGTH];
@@ -30,17 +16,30 @@ int messageIndex = 0;
 char phone[16];
 char datetime[24];
 
-GPRS gprs(PIN_TX, PIN_RX, BAUDRATE); //RX,TX,PWR,BaudRate
+GPRS gprs(PIN_TX, PIN_RX, BAUDRATE); //RX,TX,BaudRate
 
 void setup() {
     gprs.checkPowerUp();
     Serial.begin(9600);
+
     while (!gprs.init()) {
-        Serial.print("init error\r\n");
         delay(1000);
+        Serial.println("Initialization failed!");
     }
-    delay(3000);
-    Serial.println("Init Success, please send SMS message to me!");
+
+    while (!gprs.isNetworkRegistered()) {
+        delay(1000);
+        Serial.println("Network has not registered yet!");
+    }
+
+    Serial.println("gprs initialize done!");
+    Serial.println("start to send message ...");
+
+    if (gprs.sendSMS(PHONE_NUMBER, MESSAGE)) { //define phone number and text
+        Serial.print("Send SMS Succeed!\r\n");
+    } else {
+        Serial.print("Send SMS failed!\r\n");
+    }
 }
 
 void loop() {
